9cc94be7444bb10b65a49723bbd264e1c2e708a4
mmwvh <mmwvh@localhost.localdomain>
2015-10-07 11:14:39 +0200

    Initial commit

---
+++ assignment1/instance-template.pl
@@ -0,0 +1,31 @@
+% ------------------------- Problem Instance --------------------------
+% This file is a template for a problem instance: the definition of an
+% initial state and of a goal. 
+
+% debug(on).	% need additional debug information at runtime?
+
+
+
+% --- Load domain definitions from an external file -------------------
+
+:- [domain-template].		% Replace with the domain for this problem
+
+
+
+
+% --- Definition of the initial state ---------------------------------
+
+
+
+
+
+
+% --- Goal condition that the planner will try to reach ---------------
+
+goal(S) :-					% fill in the goal definition
+
+
+
+
+% ---------------------------------------------------------------------
+% ---------------------------------------------------------------------
