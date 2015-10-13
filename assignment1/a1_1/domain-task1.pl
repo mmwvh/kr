699bd79ae8a2930bada1b09cbffbe1fa317e6578
mmwvh <mmwvh@localhost.localdomain>
2015-10-13 15:59:52 +0200

    Door Ylja: implementatie voor On(), Clear(), Move() en een opzetje naar Push().

---
+++ assignment1/a1_1/domain-task1.pl
@@ -0,0 +1,63 @@
+% ------------------------- Domain Definition -------------------------
+% This file describes a planning domain: a set of predicates and
+% fluents that describe the state of the system, a set of actions and
+% the axioms related to them. More than one problem can use the same
+% domain definition, and therefore include this file
+
+
+% --- Cross-file definitions ------------------------------------------
+% marks the predicates whose definition is spread across two or more
+% files
+
+:- multifile on/3, clear/1.
+
+
+
+% --- Primitive control actions ---------------------------------------
+% this section defines the name and the number of parameters of the
+% actions available to the planner
+%
+primitive_action( move(_,_) ).	% underscore means `anything'
+primitive_action(push(_,_)).
+
+
+
+
+
+
+% --- Precondition for primitive actions ------------------------------
+% describe when an action can be carried out, in a generic situation S
+%
+% poss( doSomething(...), S ) :- preconditions(..., S).
+
+poss(push(Agent,Direction), S) :-
+	on(Agent, Loc, S),
+	connected(Loc, Loc2, Direction),
+	on(_, Loc2, S),
+	connected(Loc2, Loc3, Direction),
+	clear(Loc3, S).
+poss(move(Agent, Direction),S) :-
+	on(Agent, Loc, S),
+	connected(Loc, Loc2, Direction),
+	clear(Loc2, S).
+
+
+% --- Successor state axioms ------------------------------------------
+% describe the value of fluent based on the previous situation and the
+% action chosen for the plan.
+%
+% fluent(..., result(A,S)) :- positive; previous-state, not(negative).
+
+on(Object, Loc, result(A,S)) :-
+	A = move(Object, Direction), on(Object, Loc_i, S), connected(Loc_i,Loc, Direction);
+	on(Object, Loc, S), not(A= move(Object, _)).
+
+clear(Loc, result(A,S)):-
+	A = move(Object,_), on(Object, Loc, S);
+	not(A = move(Object, Direction), on(Object, Loc_i, S), connected(Loc_i,Loc, Direction), clear(Loc,S)).
+
+
+
+
+% ---------------------------------------------------------------------
+% ---------------------------------------------------------------------
