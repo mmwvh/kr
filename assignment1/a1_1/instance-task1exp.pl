bc1166364935454f1ad308c8d554d6accef34e41
mmwvh <mmwvh@localhost.localdomain>
2015-10-23 09:51:04 +0200

    reset connections with directions

--- assignment1/a1_1/instance-task1exp.pl
+++ assignment1/a1_1/instance-task1exp.pl
@@ -14,36 +14,42 @@
 
 % --- Definition of the initial state ---------------------------------
 
-connect(loc1-1, loc1-2).
-connect(loc1-2, loc1-1).
-connect(loc1-2, loc1-3).
-connect(loc1-3, loc1-2).
-connect(loc1-3, loc1-4).
-connect(loc1-4, loc1-3).
-connect(loc2-1, loc2-2).
-connect(loc2-2, loc2-1).
-connect(loc2-2, loc2-3).
-connect(loc2-3, loc2-2).
-connect(loc2-3, loc2-4).
-connect(loc2-4, loc2-3).
-connect(loc3-1, loc3-2).
-connect(loc3-2, loc3-1).
-connect(loc3-2, loc3-3).
-connect(loc3-3, loc3-2).
-connect(loc1-1, loc2-1).
-connect(loc2-1, loc1-1).
-connect(loc2-1, loc3-1).
-connect(loc3-1, loc2-1).
-connect(loc1-2, loc2-2).
-connect(loc2-2, loc1-2).
-connect(loc2-2, loc3-2).
-connect(loc3-2, loc2-2).
-connect(loc1-3, loc2-3).
-connect(loc2-3, loc1-3).
-connect(loc2-3, loc3-3).
-connect(loc3-3, loc2-3).
-connect(loc1-4, loc2-4).
-connect(loc2-4, loc1-4).
+connect(loc1-1, loc1-2, n).
+connect(loc1-2, loc1-1, s).
+connect(loc1-2, loc1-3, n).
+connect(loc1-3, loc1-2, s).
+connect(loc1-3, loc1-4, n).
+connect(loc1-4, loc1-3, s).
+
+connect(loc2-1, loc2-2, n).
+connect(loc2-2, loc2-1, s).
+connect(loc2-2, loc2-3, n).
+connect(loc2-3, loc2-2, s).
+connect(loc2-3, loc2-4, n).
+connect(loc2-4, loc2-3, s).
+
+connect(loc3-1, loc3-2, n).
+connect(loc3-2, loc3-1, s).
+connect(loc3-2, loc3-3, n).
+connect(loc3-3, loc3-2, s).
+
+connect(loc1-1, loc2-1, e). 
+connect(loc2-1, loc1-1, w).
+connect(loc2-1, loc3-1, e).
+connect(loc3-1, loc2-1, w).
+ 
+connect(loc1-2, loc2-2, e).
+connect(loc2-2, loc1-2, w).
+connect(loc2-2, loc3-2, e). 
+connect(loc3-2, loc2-2, w).
+
+connect(loc1-3, loc2-3, e).
+connect(loc2-3, loc1-3, w).
+connect(loc2-3, loc3-3, e).
+connect(loc3-3, loc2-3, w).
+
+connect(loc1-4, loc2-4, e).
+connect(loc2-4, loc1-4, w).
 
 clear(loc1-1, s0).
 clear(loc1-2, s0).
@@ -52,9 +58,6 @@ clear(loc1-4, s0).
 clear(loc2-4, s0).
 clear(loc3-1, s0).
 clear(loc3-3, s0).
-
-%connect(Loc2, Loc1):-
-%	connect(Loc1, Loc2).
 
 crate(blockA).
 crate(blockB).
