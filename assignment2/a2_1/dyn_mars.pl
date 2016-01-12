cdedfbd2074d4b9ec87f6516db3767a715e301e1
mmwvh <wvhoorn92@gmai.com>
2016-01-12 15:55:41 +0100

    opdracht 1 bijna af, afwachtend op mail van maarten mbt dyn mars

--- assignment2/a2_1/dyn_mars.pl
+++ assignment2/a2_1/dyn_mars.pl
@@ -1,7 +1,17 @@
-0.80::conditions(S, N) :- conditions(S-1, N).
-0.10::conditions(S, N+1) :- conditions(S-1, N), N<3.
-0.10::conditions(S, N-1) :- conditions(S-1, N), N>0.
+life(0).
+conditions(0, 3).
 
-evidence(conditions(0, 3)).
+%calcP(N, P):- N>0, P  is 1-(1/(N+1)).
+calcP(N, 0).
 
-query(conditions(1, 2)).
+0.80::conditions(S, N); 
+0.10::conditions(S, Npl); 
+0.10::conditions(S, Nmin) :- 
+Smin is S-1,
+conditions(Smin, N),
+Nmin is N-1, Npl is N+1,
+N<3, N>0.
+
+P::life(S) :- Smin is S-1, life(Smin), conditions(Smin, N), calcP(N, P).
+
+query(life(1)).
--- assignment2/a2_1/mars_q-life_e-spec.pl
+++ assignment2/a2_1/mars_q-life_e-spec.pl
@@ -1,15 +1,13 @@
 expert(0.05, 0.10, 0.05).
-%expert(0.15, 0.20, 0.15).
-
-measurenut(V).
+expert(0.15, 0.20, 0.15).
 
 1.00::process.
 X::water :- expert(X,Y,Z).
 Y::atmos :- expert(X,Y,Z).
 Z::nutri :- expert(X,Y,Z).
 
-0.90::specnut :- nutri, measurenut(V).
-0.10::specnut :- \+nutri, measurenut(V).
+0.90::specnut(M) :- nutri.
+0.10::specnut(M) :- \+nutri.
 
 0.68::geo :- process, water.
 0.20::geo :- process, \+water.
@@ -23,7 +21,8 @@ Z::nutri :- expert(X,Y,Z).
 0.97::specmeth :- meth.
 0.08::specmeth :- \+meth.
 
-evidence(measurenut(a), true).
-
+evidence(specnut(1), true).
+evidence(specnut(2), true).
+evidence(specnut(3), false).
 
 query(life).
