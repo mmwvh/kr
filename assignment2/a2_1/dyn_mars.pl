fe1a77329a0894ef9631e9069853bfe7853edcbc
mmwvh <wvhoorn92@gmai.com>
2016-01-14 09:25:44 +0100

    Aanpassingen door Maarten doorgevoerd

--- assignment2/a2_1/dyn_mars.pl
+++ assignment2/a2_1/dyn_mars.pl
@@ -1,17 +1,17 @@
 life(0).
 conditions(0, 3).
 
-%calcP(N, P):- N>0, P  is 1-(1/(N+1)).
-calcP(N, 0).
+calcP(N, P):- N>0, P  is 1-(1/(N+1)).
 
 0.80::conditions(S, N); 
-0.10::conditions(S, Npl); 
-0.10::conditions(S, Nmin) :- 
-Smin is S-1,
-conditions(Smin, N),
-Nmin is N-1, Npl is N+1,
+0.10::conditions(S, Np); 
+0.10::conditions(S, Nm) :- 
+S>0,
+Sm is S-1,
+conditions(Sm, N),
+Nm is N-1, Np is N+1,
 N<3, N>0.
 
-P::life(S) :- Smin is S-1, life(Smin), conditions(Smin, N), calcP(N, P).
+P::life(S) :- Sm is S-1, life(Sm), conditions(Sm, N), calcP(N, P).
 
 query(life(1)).
