7af0eb0ed18efbbdc27ab616a1eec9800f551fe6
mmwvh <wvhoorn92@gmai.com>
2016-01-18 12:43:57 +0100

    Assignment 1 material done

--- assignment2/a2_1/dyn_mars.pl
+++ assignment2/a2_1/dyn_mars.pl
@@ -3,15 +3,23 @@ conditions(0, 3).
 
 calcP(N, P):- N>0, P  is 1-(1/(N+1)).
 
-0.80::conditions(S, N); 
-0.10::conditions(S, Np); 
-0.10::conditions(S, Nm) :- 
-S>0,
-Sm is S-1,
-conditions(Sm, N),
-Nm is N-1, Np is N+1,
-N<3, N>0.
+bound0(0, 0).
+bound0(N, Nnew) :- N>0, Nnew is N-1.
 
-P::life(S) :- Sm is S-1, life(Sm), conditions(Sm, N), calcP(N, P).
+bound3(3, 3).
+bound3(N, Nnew) :- N<3, Nnew is N+1.
+
+0.80::conditions(S, N); 
+0.10::conditions(S, Npl); 
+0.10::conditions(S, Nmin) :- 
+S>0,
+Smin is S-1,
+conditions(Smin, N),
+bound0(N, Nmin),
+bound3(N, Npl).
+
+P::life(S) :- S>0, Smin is S-1, life(Smin), conditions(Smin, N), calcP(N, P).
 
 query(life(1)).
+query(life(2)).
+query(life(5)).
